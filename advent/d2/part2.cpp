6efba00961ddca8d01457f6e88aaa26057ca9940
Gretiks <156473423+Gretiks@users.noreply.github.com>
2024-12-04 23:08:01 +0100

    Add files via upload

--- advent/d2/part2.cpp
+++ advent/d2/part2.cpp
@@ -10,20 +10,35 @@ bool rosnie(vector<int> v)
         roznica = v[i+1] - v[i];
         if(roznica < 0 || roznica == 0 || roznica > 3)
         {
-            for(int j = 0; j <= i; j++)
+            for(int j = 0; j < i; j++)
                 v2.push_back(v[j]);
 
-            for(int j = i+2; j < v.size(); j++)
+            for(int j = i+1; j < v.size(); j++)
                 v2.push_back(v[j]);
+
             roznica = 0;
-
-            for(int j = 0; j < v2.size(); j++)
+            if(v2.size() == v.size())
             {
-                roznica = v2[j+1] - v2[j];
+                v2.clear();
+                for(int j = 1; j < v.size(); j++)
+                    v2.push_back(v[j]);
+            }
+            else if(i == v.size()-2)
+            {
+                v2.clear();
+                for(int j = 0; j < v.size()-1; j++)
+                    v2.push_back(v[j]);
+            }
+
+
+            for(int j = 0; j < v2.size()-1; j++)
+            {
+                roznica = v2[j+1] - v[j];
                 if(roznica < 0 || roznica == 0 || roznica > 3) return false;
             }
-        }   
+        }
     }
+
     return true;
 }
 
@@ -36,20 +51,35 @@ bool maleje(vector<int> v)
         roznica = v[i] - v[i+1];
         if(roznica < 0 || roznica == 0 || roznica > 3)
         {
-            for(int j = 0; j <= i; j++)
+            for(int j = 0; j < i; j++)
                 v2.push_back(v[j]);
-
-            for(int j = i+2; j < v.size(); j++)
+;
+            for(int j = i+1; j < v.size(); j++)
                 v2.push_back(v[j]);
 
             roznica = 0;
-            for(int j = 0; j < v2.size(); j++)
+            if(v2.size() == v.size())
+            {
+                v2.clear();
+                for(int j = 1; j < v.size(); j++)
+                    v2.push_back(v[j]);
+            }
+            else if(i == v.size()-2)
+            {
+                v2.clear();
+                for(int j = 0; j < v.size()-1; j++)
+                    v2.push_back(v[j]);
+            }
+
+
+            for(int j = 0; j < v2.size()-1; j++)
             {
                 roznica = v2[j] - v2[j+1];
                 if(roznica < 0 || roznica == 0 || roznica > 3) return false;
             }
         }
     }
+
     return true;
 }
 
